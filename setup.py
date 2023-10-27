e72886577a1536a1c73dacdff54cf4e0157682be
Amir sohel sheikh <amirsohel059@gmail.com>
2023-10-28 01:45:06 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return requirements
+
+    '''
+    Hyp_E = "-e ."
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        [req.replace("\n","") for req in requirements]
+
+        if Hyp_E in requirements:
+            requirements.remove(Hyp_E)
+
+
+
+
+
+setup(
+    name='mlprojects',
+    version='0.0.1',
+    author_email='sohelamir051@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
